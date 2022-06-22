2bd7564e31e09ff7698da293fb476c23230c7fb2
rodrigo <rodrigo.rgtic@gmail.com>
2022-06-22 10:20:03 -0300

    get lessons api

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,26 +1,49 @@
 import { CheckCircle, Lock } from "phosphor-react";
+import { format, isPast } from "date-fns";
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Lesson() {
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: ptBR,
+  });
+
   return (
     <a href="#">
       <span className="text-gray-300">
-        Domingo • 20 de junho • 19h00
+        {availableDateFormatted}
       </span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-            <CheckCircle size={20} />
-            Conteúdo liberado
-          </span>
+          {
+            isLessonAvailable ? (
+              <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                <CheckCircle size={20} />
+                Conteúdo liberado
+              </span>
+            ) : (
+              <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                <Lock size={20} />
+                Em breve
+              </span>
+            )
+          }
 
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-            AO VIVO
+            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
         <strong className="text-gray-200 mt-5 block">
-          Abertura do evento Ignite labs
+          {props.title}
         </strong>
       </div>
     </a>
